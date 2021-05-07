cc1622fc087a01b4ef3e776d05757b5e9733ae9a
Karanchoudhary <karanchoudhary0826@gmail.com>
2021-05-07 18:49:27 +0530

    Update script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,38 @@
+onst auth = firebase.auth();
+
+const whenSignedIn = document.getElementById("whenSignedIn");
+const whenSignedOut = document.getElementById("whenSignedOut");
+const signup = document.getElementById("signup");
+
+const signInBtn = document.getElementById("signInBtn");
+const signOutBtn = document.getElementById("signOutBtn");
+
+const userDetails = document.getElementById("userDetails");
+// const nav = document.getElementById("nav");
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+/// Sign in event handlers
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // signed in
+    whenSignedIn.hidden = false;
+    signup.hidden = true;
+    userDetails.innerHTML = `<img src="${user.photoURL}"><div class="userInfo"><h3>${user.displayName}</h3><p>${user.email}</p></div>`;
+    document.getElementById("signup").style.display = "none";
+  } else {
+    // not signed in
+    whenSignedIn.hidden = true;
+    signup.hidden = false;
+    userDetails.innerHTML = "";
+  }
+});
+
 var transcript,a,date,hours;
 var greetings = ['hello','hey there','hi','howdy','hola','hi there','hey']; // A list of greetings that the assistant replies when the transcript is equal to "hello"
 var music_cmnds = ['music','tune','songs','rhythm'];
