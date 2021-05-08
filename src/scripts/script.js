40dcbc91461882aa56436a1d51319c81b0d15acf
Karanchoudhary <karanchoudhary0826@gmail.com>
2021-05-08 18:46:22 +0530

    Update script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -19,7 +19,7 @@ auth.onAuthStateChanged(user => {
         // signed in
         whenSignedIn.hidden = false;
         whenSignedOut.hidden = true;
-        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+        userDetails.innerHTML = `<img src="${user.photoURL}"><p>User ID: ${user.displayName}</p>`;
     } else {
         // not signed in
         whenSignedIn.hidden = true;
