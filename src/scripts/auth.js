66e5a932821edfc5ae459bd214c4462c2dea9c2b
Karanchoudhary <karanchoudhary0826@gmail.com>
2021-05-09 13:27:33 +0530

    Initial Commit

---
+++ src/scripts/auth.js
@@ -0,0 +1,15 @@
+auth.onAuthStateChanged((user) => {
+    if (user) {
+      // signed in
+      whenSignedIn.hidden = false;
+      whenSignedOut.hidden = true;
+      userDetails.innerHTML = `<img src="${user.photoURL}"><div class="userInfo"><h3>${user.displayName}</h3><p>${user.email}</p></div>`;
+      document.getElementById("whenSignedOut").style.display = "none";
+    } else {
+      // not signed in
+      whenSignedIn.hidden = true;
+      whenSignedOut.hidden = false;
+      userDetails.innerHTML = "";
+      document.getElementById("whenSignedOut").style.display = "block";
+    }
+  });
