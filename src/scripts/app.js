57b7e52504ac1aefbee0f418858b7e52e4c6d42b
Karanchoudhary <karanchoudhary0826@gmail.com>
2021-05-09 18:38:56 +0530

    Initial Commit

---
+++ src/scripts/app.js
@@ -0,0 +1,15 @@
+body.onload = () => {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =
+    h + ":" + m;
+    var t = setTimeout(startTime, 1000);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
