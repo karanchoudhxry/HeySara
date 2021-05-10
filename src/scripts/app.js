b6477befa2fdf151b5fa2040a3a88e9c68e8c10c
Karanchoudhary <karanchoudhary0826@gmail.com>
2021-05-10 17:36:33 +0530

    Update app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,17 @@
-const body = document.body;
+const body = document.body.addEventListener("load", startTime);
 
-body.onload = () => {
+function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('time').innerHTML =
-    h + ":" + m;
+    document.getElementById('time').innerHTML = h + ":" + m;
     var t = setTimeout(startTime, 1000);
   }
-  function checkTime(i) {
+
+function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
   }
