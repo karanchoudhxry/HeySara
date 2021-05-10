41c131459de42e0544cc619dbd55bf31d3e517d4
Karanchoudhary <karanchoudhary0826@gmail.com>
2021-05-10 17:59:25 +0530

    Update app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,7 +5,7 @@ function startTime() {
   var s = today.getSeconds();
   m = checkTime(m);
   s = checkTime(s);
-  document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+  document.getElementById('time').innerHTML = h + ":" + m;
   var t = setTimeout(startTime, 1000);
 }
 function checkTime(i) {
