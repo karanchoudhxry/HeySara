6e79253acdcde3a808069cae6e881e80737d6149
Pseudo-Pythonic <pseudopythonic@gmail.com>
2021-05-11 11:13:27 +0530

    Check: Greetings to the User

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,29 @@
 const body = document.body;
-
+var today = new Date();
+var h = today.getHours();
+var m = today.getMinutes();
+var s = today.getSeconds();
 body.onload = () => {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('time').innerHTML =
     h + ":" + m;
     var t = setTimeout(startTime, 1000);
+    greetings()
   }
   function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
+  }
+
+  function greetings(){
+    if( h < 12){
+      responsiveVoice.speak("Good Morning User!")
+    }
+    else if(h >= 12 && h < 16){
+      responsiveVoice.speak("Good Afternoon User!")
+    }
+    else if(h >= 16){
+      responsiveVoice.speak("Good evening User!")
+    }
   }
