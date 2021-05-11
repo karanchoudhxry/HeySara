0d2ad2232a3d82c052f7f6c04fba0b3ce2943373
Pseudo-Pythonic <pseudopythonic@gmail.com>
2021-05-11 11:15:41 +0530

    Check: Checking for Greetings

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,29 +1,28 @@
+function greetings() {
+  if (h < 12) {
+    responsiveVoice.speak("Good Morning User!");
+  } else if (h >= 12 && h < 16) {
+    responsiveVoice.speak("Good Afternoon User!");
+  } else if (h >= 16) {
+    responsiveVoice.speak("Good evening User!");
+  }
+}
+
 const body = document.body;
 var today = new Date();
 var h = today.getHours();
 var m = today.getMinutes();
 var s = today.getSeconds();
 body.onload = () => {
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('time').innerHTML =
-    h + ":" + m;
-    var t = setTimeout(startTime, 1000);
-    greetings()
-  }
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
-
-  function greetings(){
-    if( h < 12){
-      responsiveVoice.speak("Good Morning User!")
-    }
-    else if(h >= 12 && h < 16){
-      responsiveVoice.speak("Good Afternoon User!")
-    }
-    else if(h >= 16){
-      responsiveVoice.speak("Good evening User!")
-    }
-  }
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("time").innerHTML = h + ":" + m;
+  var t = setTimeout(startTime, 1000);
+  greetings();
+};
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
